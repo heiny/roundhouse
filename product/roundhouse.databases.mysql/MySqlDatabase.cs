73672de9f4595154f3f1c981b7e434224c86f5a1
heiny <brent.heinz@gmail.com>
2015-10-21 08:04:03 -0700

    Add ability to use dynamic port via connection string and fix chucknorris/roundhouse#117

--- product/roundhouse.databases.mysql/MySqlDatabase.cs
+++ product/roundhouse.databases.mysql/MySqlDatabase.cs
@@ -11,11 +11,15 @@ namespace roundhouse.databases.mysql
     {
         private readonly MySqlAdoNetProviderResolver my_sql_ado_net_provider_resolver;
 
+        public string port { get; set; }
+
         public MySqlDatabase()
         {
             my_sql_ado_net_provider_resolver = new MySqlAdoNetProviderResolver();
             my_sql_ado_net_provider_resolver.register_db_provider_factory();
             my_sql_ado_net_provider_resolver.enable_loading_from_merged_assembly();
+
+            port = "3306";
         }
 
         public override bool split_batch_statements
@@ -44,6 +48,11 @@ namespace roundhouse.databases.mysql
                     {
                         database_name = part.Substring(part.IndexOf("=") + 1);
                     }
+
+                    if (string.IsNullOrEmpty(database_name) && (part.to_lower().Contains("port")))
+                    {
+                        port = part.Substring(part.IndexOf("=") + 1);
+                    }
                 }
             }
 
@@ -59,7 +68,7 @@ namespace roundhouse.databases.mysql
                 var password = InteractivePrompt.get_password("root", configuration_property_holder);
                 InteractivePrompt.write_footer();
                 
-                connection_string = build_connection_string(server_name, database_name, user_name, password);
+                connection_string = build_connection_string(server_name, database_name, port, user_name, password);
             }
 
             configuration_property_holder.ConnectionString = connection_string;
@@ -81,16 +90,21 @@ namespace roundhouse.databases.mysql
             provider = "MySql.Data.MySqlClient";
         }
 
-        private static string build_connection_string(string server_name, string database_name, string user_name, string password)
+        private static string build_connection_string(string server_name, string database_name, string port, string user_name, string password)
         {
-            return string.Format("Server={0};Database={1};Port=3306;Uid={2};Pwd={3};", server_name, database_name, user_name, password);
+            // NOTE: allow user variables was a change
+            return string.Format("Server={0};Database={1};Port={2};Uid={3};Pwd={4};AllowUserVariables=true;", 
+                server_name, database_name, port, user_name, password);
         }
 
         public override string create_database_script()
         {
             return string.Format(
-                @"CREATE DATABASE IF NOT EXISTS `{0}`;",
-                database_name);
+@"SET @had := (SELECT COUNT(1) FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '{0}');
+CREATE DATABASE IF NOT EXISTS {0};
+SET @has := (SELECT COUNT(1) FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '{0}');
+SELECT (@had = 0 AND @has = 1) as created;"
+            , database_name);
         }
 
         public override string delete_database_script()
